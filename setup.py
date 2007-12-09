c3e5f266abfdded59a5a460395e1880bf892ff9a
gak <gak@3cf1dcd9-6da1-4040-8ed6-b0d89599595e>
2007-12-09 07:26:06 +0000

    trunk updates

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='pygooglechart',
+      version='0.1.0',
+      py_modules=['pygooglechart'],
+      description='A complete Python wrapper for the Google Chart API',
+      author='Gerald Kaszuba',
+      author_email='gerald@geraldkaszuba.com',
+      url='http://pygooglechart.slowchop.com/',
+      )
