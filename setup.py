90f95fe288d07bd21d68d0917cbd83a1874cd814
gak <gak@3cf1dcd9-6da1-4040-8ed6-b0d89599595e>
2007-12-15 23:49:21 +0000

    Added more metadata in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from distutils.core import setup
 from pygooglechart import __version__
 
 setup(name='pygooglechart',
-      version=__version__,
-      py_modules=['pygooglechart'],
-      description='A complete Python wrapper for the Google Chart API',
-      author='Gerald Kaszuba',
-      author_email='gerald@geraldkaszuba.com',
-      url='http://pygooglechart.slowchop.com/',
-      )
+    version=__version__,
+    py_modules=['pygooglechart'],
+    description='A complete Python wrapper for the Google Chart API',
+    author='Gerald Kaszuba',
+    author_email='gerald@geraldkaszuba.com',
+    url='http://pygooglechart.slowchop.com/',
+    download_url='http://pygooglechart.slowchop.com/files/download/' \
+        'pygooglechart-%s.tar.gz' % __version__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    )
 
