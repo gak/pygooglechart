d473b3a692b4c5964a85ee59a58d3de0bb7adf32
gak <gak@3cf1dcd9-6da1-4040-8ed6-b0d89599595e>
2008-04-26 04:49:19 +0000

    Updating dist script to copy files to the server

--- scripts/make_dist.sh
+++ scripts/make_dist.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+rm -fr dist
 python setup.py sdist --formats=gztar,zip
 python setup.py bdist --formats=wininst
-python setup.py register
+#python setup.py register
+
+scp dist/* slowchop.com:/home/httpd/html/slowchop.com/pygooglechart/download/
 
