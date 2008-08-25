8d3e594fe5a5766de39cd4d7f93fc9e822960b1c
gak <gak@3cf1dcd9-6da1-4040-8ed6-b0d89599595e>
2008-08-25 08:03:22 +0000

    - Added stripes and grid line chart to examples

--- examples/line.py
+++ examples/line.py
@@ -7,6 +7,7 @@ import math
 ROOT = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(ROOT, '..'))
 
+from pygooglechart import Chart
 from pygooglechart import SimpleLineChart
 from pygooglechart import XYLineChart
 from pygooglechart import SparkLineChart
@@ -59,7 +60,7 @@ def sparklines():
 
 def fill():
 
-    # Set the vertical range to 0 to 50
+    # Set the vertical range from 0 to 50
     max_y = 50
     chart = SimpleLineChart(200, 125, y_range=[0, max_y])
 
@@ -95,6 +96,44 @@ def fill():
 
     chart.download('line-fill.png')
 
+def stripes():
+    
+    # Set the vertical range from 0 to 100
+    max_y = 100
+
+    # Chart size of 200x125 pixels and specifying the range for the Y axis
+    chart = SimpleLineChart(200, 125, y_range=[0, max_y])
+
+    # Add the chart data
+    data = [
+        32, 34, 34, 32, 34, 34, 32, 32, 32, 34, 34, 32, 29, 29, 34, 34, 34, 37,
+        37, 39, 42, 47, 50, 54, 57, 60, 60, 60, 60, 60, 60, 60, 62, 62, 60, 55,
+        55, 52, 47, 44, 44, 40, 40, 37, 34, 34, 32, 32, 32, 31, 32
+    ]
+    chart.add_data(data)
+    
+    # Set the line colour to blue
+    chart.set_colours(['0000FF'])
+
+    # Set the vertical stripes
+    chart.fill_linear_stripes(Chart.CHART, 0, 'CCCCCC', 0.2, 'FFFFFF', 0.2)
+
+    # Set the horizonal dotted lines
+    chart.set_grid(0, 25, 5, 5)
+
+    # The Y axis labels contains 0 to 100 skipping every 25, but remove the
+    # first number because it's obvious and gets in the way of the first X
+    # label.
+    left_axis = range(0, max_y + 1, 25)
+    left_axis[0] = ''
+    chart.set_axis_labels(Axis.LEFT, left_axis)
+
+    # X axis labels
+    chart.set_axis_labels(Axis.BOTTOM, \
+        ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'])
+
+    chart.download('line-stripes.png')
+
 def main():
     simple_random()
     xy_random()
@@ -102,6 +141,7 @@ def main():
     xy_circle()
     sparklines()
     fill()
+    stripes()
 
 if __name__ == '__main__':
     main()
